407ec0b7fcb0c9d42aac65e39ed280f04703765d
Segimara <vadymgdm@gmail.com>
2023-05-29 15:20:32 +0300

    wtf

---
+++ .history/js/animated-router_20230528222122.js
@@ -0,0 +1,15 @@
+const pageContainer = document.getElementById('pageContainer');
+
+function loadPage(url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      pageContainer.innerHTML = html;
+      animatePageTransition();
+    })
+    .catch(error => console.error(error));
+}
+
+function animatePageTransition() {
+  // Add your animation logic here
+}
